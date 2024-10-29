3fd160ea2211816f0465353b63bb1edf55e55fc4
KazzModan <kklopov64@gmail.com>
2024-10-30 00:35:12 +0200

    Introduce ApiRequestExecutor

---
+++ WeatherApp.Domain/REST/IApiRequestExecutor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WeatherApp.Domain.REST
+{
+    public interface IApiRequestExecutor
+    {
+        Task<TResponse> GetAsync<TResponse>(string request);
+    }
+}
---
+++ WeatherApp.Infrastructure/REST/ApiRequestExecutor.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherApp.Domain.REST;
+
+namespace WeatherApp.Infrastructure.REST
+{
+    public class ApiRequestExecutor : IApiRequestExecutor
+    {
+        private readonly Uri  _baseAddress = new Uri("http://dataservice.accuweather.com/");
+        private readonly string apiKey = "mw67UsQIG9Ifq6qiVGl15dqiyzRGhxbR";
+
+        public async Task<TResponse>  GetAsync<TResponse>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = _baseAddress;
+                var httpResponseMessage = await client.GetAsync(request);
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<TResponse>(content);
+                return response;
+            };
+        }
+    }
+}
